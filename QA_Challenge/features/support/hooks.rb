8199dc69ce1e8f4085a17454f27228a4fffd21b5
Rafael Fragoso <rafahrfragoso@gmail.com>
2018-10-29 00:10:47 -0300

    inclusão da geraçãod e relatório

--- QA_Challenge/features/support/hooks.rb
+++ QA_Challenge/features/support/hooks.rb
@@ -5,3 +5,16 @@ end
 After('@logout') do
     click_link 'Log me out'
 end
+
+After do |scenario|
+    #atribunindo 'scenario' uma variável nome_cenario e utilizando uma regex para retirar caracteres especiais
+    nome_cenario = scenario.name.gsub(/[^A-Za-z0-9 ]/, '')
+    #atribuindo nome_cenario a ele mesmo e trocando todos os espaços em branco por '_' e colocando tudo em menusculo
+    nome_cenario = nome_cenario.gsub(' ', '_').downcase!
+    #criando diretório de screenshot e atribuindo a variável nome_cenario dinamicamente ao screenshot que será feito
+    screenshot = "log/screenshots/#{nome_cenario}.png"
+    #utilizando capybara para tirar o screenshot passando a variável 'screenshot' como parametro com o caminho e nome do screnshot
+    page.save_screenshot(screenshot) 
+    #anexando screenshots aos report
+    embed(screenshot, 'image/png', 'Evidencia')
+end
